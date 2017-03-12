5f0ac35d20e3ecafed73583f0cbf189daa86e570
Vladislav Guleaev <guleaev.vlad@gmail.com>
2017-03-12 10:22:11 +0200

    Changes to models

--- src/ToWatch/Controllers/Web/AuthController.cs
+++ src/ToWatch/Controllers/Web/AuthController.cs
@@ -2,15 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ToWatch.Models;
+using ToWatch.ViewModels;
 
 namespace ToWatch.Controllers.Web
 {
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        private SignInManager<AppUser> signInManager;
+
+        public AuthController(SignInManager<AppUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var signInResult = await signInManager.PasswordSignInAsync(model.Username, model.Password, true, false);
+
+                if (signInResult.Succeeded)
+                {
+                    if (string.IsNullOrWhiteSpace(returnUrl))
+                    {
+                        return RedirectToAction("Index", "App");
+                    }
+
+                    return Redirect(returnUrl);
+                }
+                ModelState.AddModelError("", "Username or password is incorrect");
+            }
+
+            return View();
+        }
+
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await signInManager.SignOutAsync();
+            }
+
+            return RedirectToAction("Index", "App");
         }
     }
 }
--- src/ToWatch/Models/AppUser.cs
+++ src/ToWatch/Models/AppUser.cs
@@ -7,6 +7,8 @@ namespace ToWatch.Models
 {
     public class AppUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public ICollection<SavedMovie> WatchList { get; set; }
     }
 }
