3032133d17b563cc9a24b7c595b4c893c4870bae
Vladislav Guleaev <guleaev.vlad@gmail.com>
2017-03-12 13:15:20 +0200

    Added a migration

---
+++ src/ToWatch/Migrations/20170312111310_DeletedFirstLastName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToWatch.Migrations
+{
+    public partial class DeletedFirstLastName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- src/ToWatch/Models/AppUser.cs
+++ src/ToWatch/Models/AppUser.cs
@@ -8,9 +8,6 @@ namespace ToWatch.Models
 {
     public class AppUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
         public ICollection<SavedMovie> WatchList { get; set; }
     }
 }
