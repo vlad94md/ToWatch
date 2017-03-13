3a90c5a783e744a6c10c596202b0e2f7bec7893c
Vladislav Guleaev <guleaev.vlad@gmail.com>
2017-03-13 22:17:12 +0200

    Added Api controller

---
+++ src/ToWatch/ViewModels/MovieSetApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ToWatch.ViewModels
+{
+    public class MovieSetApiResponse
+    {
+        public int Page { get; set; }
+        public IEnumerable<MovieViewModel> Results { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
