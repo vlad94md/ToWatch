dec598ec196ab2a682a281535d56a5a721133e06
Vladislav Guleaev <guleaev.vlad@gmail.com>
2017-03-21 12:47:28 +0200

    Added angular

---
+++ src/ToWatch/wwwroot/js/customControls.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("customControls", []).directive("loading", loadingAnimation);
+
+    function loadingAnimation() {
+        return {
+            scope: {
+                show: "=displayWhen"
+            },
+            restrict: "E",
+            templateUrl: "/views/loadingMovies.html"
+        };
+    };
+})();
