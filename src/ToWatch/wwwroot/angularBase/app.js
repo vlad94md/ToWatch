df5e27defd1a4f29d771a149700a0e914183931c
SergheiTibulschi <Serghei.Tibulschii@endava.com>
2017-03-21 12:10:58 +0200

    routing otpizhen

--- src/ToWatch/wwwroot/angularBase/app.js
+++ src/ToWatch/wwwroot/angularBase/app.js
@@ -1,15 +1,21 @@
 ﻿(() => {
     let app = angular.module("main", ["ngRoute"]);
 
-    app.config(($routeProvider) => {
+    app.config(['$routeProvider', '$locationProvider', function AppConfig($routeProvider, $locationProvider) {
+
         $routeProvider
-        .when("/", {
-            templateUrl: "/angularBase/views/upcomingView.html",
-            controller: "upcommingCtrl"
-        })
         .when("/toprated", {
             templateUrl: "/angularBase/views/topRatedView.html",
             controller: "topRatedCtrl"
+        })
+        .when("/", {
+            templateUrl: "/angularBase/views/upcomingView.html",
+            controller: "upcommingCtrl"
         });
-    });
+
+        // enable html5Mode for pushstate ('#'-less URLs)
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix('!');
+
+    }]);
 })();
